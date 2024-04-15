2517f827ee94fb626ecd521ec106f9095fc1723a
MaryamAgazada <maryam.agazadaa@gmail.com>
2024-04-15 22:00:07 +0400

    second commit

--- script.js
+++ script.js
@@ -4,21 +4,31 @@ const textInput=document.querySelector(".textInput")
 const titleInput=document.querySelector(".titleInput")
 const createCard=document.querySelector(".create__card")
 const cardContainer=document.querySelector(".card__container")
-btnAlert.addEventListener("click",function showAlert () {
-   let textInpAlert=textInputAlert.value;
+btnAlert.addEventListener("click", function showAlert() {
+    let textInpAlert = textInputAlert.value;
     alert(textInpAlert);
-    localStorage.setItem("savedText",textInpAlert);
-      localStorage.getItem("savedText");
+    localStorage.setItem("savedText", textInpAlert);
+});
+window.addEventListener("load", function () {
+    let savedText = localStorage.getItem("savedText");
     if (savedText) {
-        textInpAlert = savedText;
+        textInputAlert.value = savedText;
     }
 });
-createCard.addEventListener("click",function createCard(params) {
-    let card=document.createElement("div");
-    card.classList.add("card")
-    let createTitle=titleInput.value;
-    let createText=textInput.value;
-    card.innerHTML="<h3>"+createTitle+"</h3>"+"<p>"+createText+"</p>"
-    cardContainer.appendChild(card)
 
-})
+
+
+function saveCardToLocalStorage(title, text) {
+    let cards = JSON.parse(localStorage.getItem("cards")) || [];
+    cards.push({ title: title, text: text });
+    localStorage.setItem("cards", JSON.stringify(cards));
+}
+createCard.addEventListener("click", function createCard(params) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let createTitle = titleInput.value;
+    let createText = textInput.value;
+    card.innerHTML = "<h3>" + createTitle + "</h3>" + "<p>" + createText + "</p>";
+    cardContainer.appendChild(card);
+    saveCardToLocalStorage(createTitle, createText);
+});
